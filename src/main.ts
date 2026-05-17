7538eed446d60363ca9f817a6ddb29ce2e34d58b
Marco <marco.tartaglione147@gmail.com>
2026-05-18 01:18:44 +0200

    Fixed routing

--- src/main.ts
+++ src/main.ts
@@ -23,7 +23,6 @@ bootstrapApplication(AppComponent, {
         paramsInheritanceStrategy: 'always',
         onSameUrlNavigation: 'reload',
       }),
-      withHashLocation()
     ),
     importProvidersFrom(MatNativeDateModule)
   ]
