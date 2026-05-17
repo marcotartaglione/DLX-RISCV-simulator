3b7db000fb070831d4980240a6868b6f48c8f80b
Marco <marco.tartaglione147@gmail.com>
2026-05-18 01:18:18 +0200

    Stronger format conversion

--- src/app/pipes/format.pipe.ts
+++ src/app/pipes/format.pipe.ts
@@ -7,17 +7,34 @@ import {Pipe, PipeTransform} from '@angular/core';
 export class FormatPipe implements PipeTransform {
 
   transform(n: number | string, type: 'dec' | 'bin' | 'hex' | 'oct' | string, length: number = 32): any {
-    const value = typeof n === 'string' ? Number(n.trim()) : n;
+    let value: number | string = n;
+    if (typeof n === 'string') {
+      const s = n.trim();
+      const sign = s.startsWith('-') ? -1 : 1;
+      const unsignedStr = s.startsWith('+') || s.startsWith('-') ? s.substring(1) : s;
 
-    if (!Number.isFinite(value)) {
+      if (/^0[xX]/.test(unsignedStr)) {
+        value = sign * parseInt(unsignedStr.substring(2), 16);
+      } else if (/^0[bB]/.test(unsignedStr)) {
+        value = sign * parseInt(unsignedStr.substring(2), 2);
+      } else if (/^0[oO]/.test(unsignedStr)) {
+        value = sign * parseInt(unsignedStr.substring(2), 8);
+      } else {
+        value = Number(s);
+      }
+    }
+
+    const numeric = typeof value === 'number' ? value : Number(value);
+
+    if (!Number.isFinite(numeric)) {
       return n;
     }
 
-    const unsigned = value >>> 0;
+    const unsigned = numeric >>> 0;
 
     switch (type) {
       case 'dec':
-        return (value > 268435455 || value < 0) ? unsigned : value;
+        return (numeric > 268435455 || numeric < 0) ? unsigned : numeric;
       case 'bin':
         return unsigned.toString(2).padStart(length, '0');
       case 'hex':
